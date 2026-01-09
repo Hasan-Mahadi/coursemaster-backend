eb0a0a7895b298522e710a3ff650e34e9c24b759
Hasan-Mahadi <hasanmahadihm99@gmail.com>
2026-01-09 23:54:37 +0600

    done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,9 @@ import { sendWelcomeEmail } from '../utils/email.js';
 export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
-  const userExists = await User.findOne({ email });
+  // const userExists = await User.findOne({ email });
+  // if (userExists) return res.status(400).json({ message: 'User already exists' });
+   const userExists = await User.findOne({ email });
   if (userExists) return res.status(400).json({ message: 'User already exists' });
 
   const user = await User.create({ name, email, password });
