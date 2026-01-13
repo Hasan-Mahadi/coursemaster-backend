e95e6bcc928c2a731fb866f26aeccf83abf93911
Hasan-Mahadi <hasanmahadihm99@gmail.com>
2026-01-13 23:07:00 +0600

    done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
-import User from '../models/User.js';
-import generateToken from '../utils/generateToken.js';
-import { sendWelcomeEmail } from '../utils/email.js';
+import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
+import { sendWelcomeEmail } from "../utils/email.js";
 
 // Student Registration
 export const registerUser = async (req, res) => {
@@ -8,22 +8,27 @@ export const registerUser = async (req, res) => {
 
   // const userExists = await User.findOne({ email });
   // if (userExists) return res.status(400).json({ message: 'User already exists' });
-   const userExists = await User.findOne({ email });
-  if (userExists) return res.status(400).json({ message: 'User already exists' });
+  const userExists = await User.findOne({ email });
+  if (userExists)
+    return res.status(400).json({ message: "User already exists" });
 
   const user = await User.create({ name, email, password });
-    // Send welcome email (async, no await)
-    sendWelcomeEmail(email, name).catch(err => console.error("Email send error:", err));
+  // Send welcome email (async, no await)
+  sendWelcomeEmail(email, name).catch((err) =>
+    console.error("Email send error:", err)
+  );
   if (user) {
     res.status(201).json({
       _id: user._id,
+      // name: user.name,
+      // email: user.email,
       name: user.name,
       email: user.email,
       role: user.role,
       token: generateToken(user._id, user.role),
     });
   } else {
-    res.status(400).json({ message: 'Invalid user data' });
+    res.status(400).json({ message: "Invalid user data" });
   }
 };
 
@@ -41,6 +46,6 @@ export const authUser = async (req, res) => {
       token: generateToken(user._id, user.role),
     });
   } else {
-    res.status(401).json({ message: 'Invalid email or password' });
+    res.status(401).json({ message: "Invalid email or password" });
   }
 };
